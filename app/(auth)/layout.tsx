23044f2a6808ce9f83b24a7e418fccf29d323c36
Igor_Kukric <igorkukric@gmail.com>
2023-10-15 17:38:43 +0200

    initialize Clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
+import '../globals.css'
+
 export const metadata = {
   title: "Threads",
   description: "A Next.js 13 Meta Threads Application",
@@ -16,7 +18,7 @@ export default function RootLayout({
   return (
 <ClerkProvider>
     <html lang="en">
-        <body className={inter.className}>
+        <body className={`${inter.className} bg-dark-1`}>
         {children}
         </body>
     </html>
