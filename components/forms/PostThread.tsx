386f107a9c07efa6958cf6adc12e450c12c25b27
Igor_Kukric <igorkukric@gmail.com>
2023-10-16 15:40:42 +0200

    add PostThread, update UI

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -6,7 +6,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
@@ -31,19 +31,52 @@ interface Props {
   btnTitle: string;
 }
 
-function PostThread({userId}: {userId: string}) {
-   
-    const router = useRouter();
-    const pathname = usePathname();
-  
-    const form = useForm({
-      resolver: zodResolver(ThreadValidation),
-      defaultValues: {
-        thread: '',
-        accountId: userId,
-      },
-    });
-    return <h1>Post Thread Form</h1>
+function PostThread({ userId }: { userId: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col  gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  className="no-focus border border-dark-4 bg-dark-3 text-light-1"
+                  rows={15}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+            Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
 }
 
-export default PostThread
+export default PostThread;
