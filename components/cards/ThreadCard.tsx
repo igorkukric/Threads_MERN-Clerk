253ccf8576cd8e8c6507f2b8f0af62f38ed314ee
Igor_Kukric <igorkukric@gmail.com>
2023-10-16 18:38:00 +0200

    add ThreadCard logic for comments

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,8 +34,8 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
-
   return (
     <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
       <div className="flex items-start justify-between">
@@ -62,25 +62,47 @@ const ThreadCard = ({
             <p className="mt-2 text-small-regular text-light-2">{content} </p>
 
             <div className="mt-5 flex flex-col gap-3">
-                <div className="flex gap-3.5">
-                    <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} 
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="reply"
+                    width={24}
+                    height={24}
                     className="cursor-pointer object-contain"
-                    />
-                    <Link href={`/thread/${id}`}>
-                    <Image src="/assets/reply.svg" alt="reply" width={24} height={24} 
-                    className="cursor-pointer object-contain"
-                    />
-                    </Link>
-                   
-                    <Image src="/assets/repost.svg" alt="repost" width={24} height={24} 
-                    className="cursor-pointer object-contain"
-                    />
-                    <Image src="/assets/share.svg" alt="share" width={24} height={24} 
-                    className="cursor-pointer object-contain"
-                    />
-                </div>
+                  />
+                </Link>
 
-                
+                <Image
+                  src="/assets/repost.svg"
+                  alt="repost"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="share"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+              </div>
+
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies{" "}
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
