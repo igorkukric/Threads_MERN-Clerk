06b5642bae014925ecef9022a90615bd6eae4026
Igor_Kukric <igorkukric@gmail.com>
2023-10-16 17:08:10 +0200

    Calculate the number of posts to skip  Fetch the posts that have no parents (top-level threads...)  fix next.config.js

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
@@ -6,34 +6,56 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string,
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
 
-
-
-export async function createThread({text, author, communityId, path}: Params) {
-try {
-    connectToDB()
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
 
     const createdThread = await Thread.create({
-        text,
-        author,
-        community: null,
-    })
+      text,
+      author,
+      community: null,
+    });
 
     // Update user model
-    await User.findByIdAndUpdate(author,{
-        $push: {threads: createdThread._id}
-    })
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
 
-    revalidatePath(path)
-} catch (error: any) {
-    throw new Error(`Error creating thread: ${error.message}`)
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
+  }
 }
 
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
 
-   
+  //Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch the posts that have no parents (top-level threads...)
+  const postsQuery = Thread.find({ partentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "childern",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
 }
