e5e01d2c29317ddb2090118ad867d66c250c5670
Igor_Kukric <igorkukric@gmail.com>
2023-10-21 17:37:15 +0200

    add DeleteThread compnent

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,41 +1,45 @@
-import CommunityCard from "@/components/cards/CommunityCard";
-import UserCard from "@/components/cards/UserCard";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-
-import { profileTabs } from "@/constants";
-import { fetchCommunities } from "@/lib/actions/community.actions";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
-async function Page() {
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+import CommunityCard from "@/components/cards/CommunityCard";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
-    if (!user) return null;
+  if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  // Fetch communities
-const result = await fetchCommunities({
-    searchString: '',
-    pageNumber: 1,
-    pageSize: 25
-})
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
   return (
-    <section>
-      <h1 className="head-text mb-10">Search</h1>
+    <>
+      <h1 className='head-text'>Communities</h1>
 
-      {/* Search Bar */}
+      <div className='mt-5'>
+        <Searchbar routeType='communities' />
+      </div>
 
-      <div className="mt-14 flex flex-col gap-9">
+      <section className='mt-9 flex flex-wrap gap-4'>
         {result.communities.length === 0 ? (
-            <p className="no-result">No communities</p>
-        ): (
-           <>
-           {result.communities.map((community)=> (
-            <CommunityCard 
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
                 key={community.id}
                 id={community.id}
                 name={community.name}
@@ -43,14 +47,19 @@ const result = await fetchCommunities({
                 imgUrl={community.image}
                 bio={community.bio}
                 members={community.members}
-
-            />
-           ))}
-           </> 
+              />
+            ))}
+          </>
         )}
-      </div>
-    </section>
+      </section>
+
+      <Pagination
+        path='communities'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   );
-};
+}
 
 export default Page;
